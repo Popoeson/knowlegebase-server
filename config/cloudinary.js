b6a63375310dc91357680e383ea757fd4b2568d0
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-29 07:56:14 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,4 +6,17 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+const uploadToCloudinary = (buffer, options = {}) => {
+    return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+            options,
+            (error, result) => {
+                if (error) reject(error);
+                else resolve(result);
+            }
+        );
+        uploadStream.end(buffer);
+    });
+};
+
+module.exports = { cloudinary, uploadToCloudinary };
