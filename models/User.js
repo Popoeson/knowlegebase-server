b26cf6fcd52e40e9b2eaaa431737ae56081bfa9f
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-29 10:16:02 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "First name is required"],
+            trim: true
+        },
+        otherName: {
+            type: String,
+            trim: true,
+            default: ""
+        },
+        surname: {
+            type: String,
+            required: [true, "Surname is required"],
+            trim: true
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: [6, "Password must be at least 6 characters"]
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        otp: {
+            type: String,
+            default: null
+        },
+        otpExpires: {
+            type: Date,
+            default: null
+        },
+
+        // Profile fields (filled after registration)
+        profilePhoto: {
+            type: String,
+            default: null
+        },
+        phone: {
+            type: String,
+            default: null
+        },
+        bio: {
+            type: String,
+            default: null
+        }
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+// Virtual: fullName
+userSchema.virtual("fullName").get(function () {
+    if (this.otherName) {
+        return `${this.firstName} ${this.otherName} ${this.surname}`;
+    }
+    return `${this.firstName} ${this.surname}`;
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Method: compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
