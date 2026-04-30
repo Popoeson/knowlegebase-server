0f9c3233268f4838acdaa347597212a8b6cca723
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-30 04:23:43 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,12 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 
 const uploadToCloudinary = (buffer, options = {}) => {
-    console.log("Cloudinary config check:", {
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET ? "exists" : "MISSING"
-    });
-
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
