c7a3624fd9a82c426af3fdbae5712da11d71c2e9
Samuel Popoola <samuelpopoola323@gmail.com>
2026-07-18 20:55:09 +0100

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { getProfile, updateProfile, changePassword, getUserStats } = require("../controllers/user.controller");
 const { protect } = require("../middleware/auth.middleware");
+const { moderateLimiter } = require("../middleware/rateLimit.middleware");
 const upload = require("../middleware/upload.middleware");
 
 router.get("/profile", protect, getProfile);
 router.put("/profile", protect, upload.single("profilePhoto"), updateProfile);
-router.put("/change-password", protect, changePassword);
+router.put("/change-password", protect, moderateLimiter, changePassword);
 router.get("/stats", protect, getUserStats);
 
 module.exports = router;
